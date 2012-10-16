61bc7e6353d8fa6f160575ad8a64401720c9c442
Jesly Varghese <jesly.varghese@sourcebits.com>
2012-10-16 19:59:01 +0530

    manifest file genrator added

---
+++ lib/plist_generator.rb
@@ -0,0 +1,24 @@
+require 'rubygems'
+require 'plist'
+
+class Manifest
+  attr_accessor :template_name, :build_path
+
+  def create(ipa_url,image_url,bundle_identifier,bundle_version,title)
+    @manifest = Plist::parse_xml '../resources/#{@template_name}'
+    
+    @manifest['items'][0]['assets'][0]['url'] = ipa_url
+    @manifest['items'][0]['assets'][1]['url'] = image_url
+    @manifest['items'][0]['metadata']['bundle-identifier'] = bundle_identifier
+    @manifest['items'][0]['metadata']['bundle-version'] = bundle_version
+    @manifest['items'][0]['metadata']['title'] = title
+    
+    underscored_title = title.gsub /\s/ , '_'
+    underscored_version = bundle_version.gsub /\./, '_'
+    manifest_filename = "#{underscored_title}_#{underscored_version}.plist"
+    
+    @manifest.save "#{@build_path}/#{manifest_filename}"
+    
+    return manifest_filename
+  end
+end
