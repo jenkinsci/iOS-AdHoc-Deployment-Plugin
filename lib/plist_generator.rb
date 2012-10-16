411b27c4787ddd33643c8187ed4b472704681678
_justin <justin@redatomize.com>
2012-10-17 04:56:45 +0530

    plist_generator test added

--- lib/plist_generator.rb
+++ lib/plist_generator.rb
@@ -1,23 +1,22 @@
 require 'rubygems'
 require 'plist'
 
-modules Manifest
-  attr_accessor :template_name, :build_path
+module Manifest
 
-  def create(ipa_url,image_url,bundle_identifier,bundle_version,title)
-    @manifest = Plist::parse_xml '../resources/#{@template_name}'
+  def create(ipa_url,image_url,bundle_identifier,bundle_version,title,template_name,build_path)
+    manifest = Plist::parse_xml '../resources/#{template_name}'
     
-    @manifest['items'][0]['assets'][0]['url'] = ipa_url
-    @manifest['items'][0]['assets'][1]['url'] = image_url
-    @manifest['items'][0]['metadata']['bundle-identifier'] = bundle_identifier
-    @manifest['items'][0]['metadata']['bundle-version'] = bundle_version
-    @manifest['items'][0]['metadata']['title'] = title
+    manifest['items'][0]['assets'][0]['url'] = ipa_url
+    manifest['items'][0]['assets'][1]['url'] = image_url
+    manifest['items'][0]['metadata']['bundle-identifier'] = bundle_identifier
+    manifest['items'][0]['metadata']['bundle-version'] = bundle_version
+    manifest['items'][0]['metadata']['title'] = title
     
     underscored_title = title.gsub /\s/ , '_'
     underscored_version = bundle_version.gsub /\./, '_'
     manifest_filename = "#{underscored_title}_#{underscored_version}.plist"
     
-    @manifest.save "#{@build_path}/#{manifest_filename}"
+    manifest.save "#{build_path}/#{manifest_filename}"
     
     return manifest_filename
   end
