b15d2f499ca48eabcbcb98afb46f20cbf3a1d605
_justin <justin@redatomize.com>
2012-11-30 08:19:17 +0530

    eod commit

--- lib/mailer.rb
+++ lib/mailer.rb
@@ -1,9 +1,11 @@
 #Jesly Varghese 2012
+
 #Part of OTA-Builder and Uploader Plugin For iOS Project
 #Developed as a part of CI-Project @ Sourcebits LLC
 
 #Mailer class
 #This class sends a mail to the specified set of people.
+
 require "rubygems"
 require "java"
 require "pony"
@@ -13,32 +15,24 @@ java_import hudson.tasks.Mailer
 #this could have been done in two ways, either use native java methods or go for rb methods
 #i choose the latter
 
-import javax.mail.Address;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
-
 class Mail
-
 
 	def initialize
 	end
 
 	def mail
 	end
+
 end
 
 class MailConfiguration
+
 	attr_accessor :user_name, :password, :server, :port
+
 end
 
 class SMTP<MailConfiguration
+
 	attr_accessor :mail
 
 	def initialize(mail)
@@ -56,23 +50,23 @@ class SMTP<MailConfiguration
 		Pony.mail({
 				:to => mail.to,
 				:cc => mail.cc,
-				:bcc=> mail.bcc
+				:bcc=> mail.bcc,
 
 				:from=>mail.from,
 
 				:body => mail.body,
 				:html_body => mail.html_body,
 
-				:subject => mail.subject
+				:subject => mail.subject,
 
 				:charset => mail.charset,
 				:text_part_charset => mail.text_part_charset,
 
-				:attchments => mail.attchments
+				:attchments => mail.attchments,
 
-				:headers => mail.headers
-				:sender => mail.sender
-				:reply_to => mail.reply_to
+				:headers => mail.headers,
+				:sender => mail.sender,
+				:reply_to => mail.reply_to,
 
 				:via =>:smtp,
 				:via_options =>{
@@ -80,7 +74,7 @@ class SMTP<MailConfiguration
 					:port    => @port,
 					:enable_starttls_auto => true,
 					:user_name => @user_name,
-					:password => @password
+					:password => @password,
 					:authentication => :plain
 
 				}
