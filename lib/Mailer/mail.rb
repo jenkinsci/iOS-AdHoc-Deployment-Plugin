83f594e96a73acbffc3ee264eb1324e7e4373b82
_justin <justin@redatomize.com>
2012-12-03 08:20:36 +0530

    seggregated the mailer class

---
+++ lib/Mailer/mail.rb
@@ -0,0 +1,27 @@
+#Jesly Varghese 2012
+
+#Part of OTA-Builder and Uploader Plugin For iOS Project
+#Developed as a part of CI-Project @ Sourcebits LLC
+
+#Mail Module
+#This class sends a mail to the specified set of people.
+
+#this could have been done in two ways, either use native java methods or go for rb methods
+#i choose the latter
+
+module Mail
+
+	
+
+	def mail
+		#give values to all those subtle variables needed here.
+
+		@charset = 'UTF-8'
+		@text_part_charset = 'UTF-8'
+		
+	end
+
+end
+
+
+
--- lib/mailer.rb
+++ lib/mailer.rb
@@ -7,78 +7,20 @@
 #This class sends a mail to the specified set of people.
 
 require "rubygems"
-require "java"
-require "pony"
+require "require_relative"
 
-java_import hudson.tasks.Mailer
+require_relative "Mailer/smtp_mail.rb"
+require_relative "Mailer/mail.rb"
 
-#this could have been done in two ways, either use native java methods or go for rb methods
-#i choose the latter
-
-class Mail
-
+class JenkinsMail<SMTP
+	include 'Mail'
+	
 	def initialize
+
 	end
 
 	def mail
-	end
 
-end
-
-class MailConfiguration
-
-	attr_accessor :user_name, :password, :server, :port
-
-end
-
-class SMTP<MailConfiguration
-
-	attr_accessor :mail
-
-	def initialize(mail)
-		mail_descriptor = Mail.descriptor()
-
-		@user_name = mail_descriptor.getSmtpAuthUserName()
-		@password  = mail_descriptor.getSmtpAuthPassword()
-		@server    = mail_descriptor.getSmtpServer()
-		@port      = mail_descriptor.getSmtpPort()
-		
-		@mail      = mail
-	end
-
-	def send
-		Pony.mail({
-				:to => mail.to,
-				:cc => mail.cc,
-				:bcc=> mail.bcc,
-
-				:from=>mail.from,
-
-				:body => mail.body,
-				:html_body => mail.html_body,
-
-				:subject => mail.subject,
-
-				:charset => mail.charset,
-				:text_part_charset => mail.text_part_charset,
-
-				:attchments => mail.attchments,
-
-				:headers => mail.headers,
-				:sender => mail.sender,
-				:reply_to => mail.reply_to,
-
-				:via =>:smtp,
-				:via_options =>{
-					:address => @server,
-					:port    => @port,
-					:enable_starttls_auto => true,
-					:user_name => @user_name,
-					:password => @password,
-					:authentication => :plain
-
-				}
-			})
 	end
 
 end
