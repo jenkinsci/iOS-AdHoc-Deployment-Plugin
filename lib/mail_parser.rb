695de3cf0d853b6dfbd433d1644caab2997c23e0
Jesly Varghese <jesly.varghese@surcebits.com>
2012-12-06 10:37:17 +0530

    Replaced gsub! with gsub as it was mutating the original data

--- lib/mail_parser.rb
+++ lib/mail_parser.rb
@@ -5,7 +5,7 @@ module MailParser
 	def self.substitute_variables(content)
 		replacement_table = yield
 		replacement_table.each do |replacement_entry|
-			content.gsub! /#{replacement_entry[:replace]}/, replacement_entry[:with]
+			content = content.gsub /#{replacement_entry[:replace]}/, replacement_entry[:with]
 		end
 		content
 	end
