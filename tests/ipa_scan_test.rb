00539cfc3aa4de0a977b0b57ad62eaf358f17d55
Jesly Varghese <jesly.varghese@gmail.com>
2013-05-06 18:56:49 +0530

    updated test

--- tests/ipa_scan_test.rb
+++ tests/ipa_scan_test.rb
@@ -5,10 +5,13 @@ require_relative '../lib/ipa_search.rb'
 require_relative '../lib/data_from_ipa.rb'
 
 ipa_file_data = IPAFileData.new
-file_path = "/Users/jesly/Workspace/Work/SVN/trunk/Development/Builds/Build 3/FitOrbit.ipa"
-ipa = ipa_file_data.binary_plist_path_of file_path
-plist_contents =ipa_file_data.contents_of_infoplist ipa, file_path
+
+file_path = "/Users/jesly/Desktop"
+ipa_path = IPASearch.find_in file_path
+p ipa_path
+ipa = ipa_file_data.binary_plist_path_of ipa_path
+plist_contents =ipa_file_data.contents_of_infoplist ipa, ipa_path
 plist = IPA.new plist_contents
 
 p plist.displayname
-p ipa_file_data.path_to_icon_file_with_name plist.icon, file_path
+p ipa_file_data.path_to_icon_file_with_name plist.icon, ipa_path
